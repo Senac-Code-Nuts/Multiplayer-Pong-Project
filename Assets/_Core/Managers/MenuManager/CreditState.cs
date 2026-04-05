8ab8c044e7324df92dc9d8fb02173dd6b8328672
anotter dev <148708467+anotter-random-dev@users.noreply.github.com>
2026-04-04 23:29:39 -0300

    feat: states setup
    setup a main state, a credit state and an option state

---
+++ Assets/_Core/Managers/MenuManager/CreditState.cs
@@ -0,0 +1,23 @@
+using MenuManager;
+using UnityEngine;
+
+namespace MenuManager{
+public class CreditState : MenuState
+{
+    public override void LocalStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void LocalUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnChange()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
+}
