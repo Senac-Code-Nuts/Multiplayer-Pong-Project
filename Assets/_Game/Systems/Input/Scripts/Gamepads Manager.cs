6fefafb7b6b055dda1124f80375fb69d504d0084
N3xus-Art <Filipemelgacoazevedo@gmail.com>
2026-04-04 21:19:01 -0300

    FIX (Gamepad Manager): Fiz as mudanças pedidas na revisão do pull request
    - Alterei os nomes das variáveis para se enquadrar no padrão definido
    - Adicionei a propriedade private nos métodos
    - Alterei a Lista de Gamepads para um de Array

--- Assets/_Game/Systems/Input/Scripts/Gamepads Manager.cs
+++ Assets/_Game/Systems/Input/Scripts/Gamepads Manager.cs
@@ -1,37 +1,39 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Linq;
-using System.Collections.Generic;
+using System;
 
 namespace Pong.Systems
 {
     public class GamepadsManager : MonoBehaviour
     {
-        private List<Gamepad> _gamepads = new List<Gamepad>(4);
-        private int _currentgamepadsnumber = 0;
+        private Gamepad[] _gamepads = new Gamepad[4] {null,null,null,null};
+        private int _currentGamepadsNumber = 0;
 
-        [SerializeField] private InputReader[] _inputreaders = new InputReader[4];
+        [SerializeField] private InputReader[] _inputReaders = new InputReader[4];
 
-        void OnEnable()
+        private void OnEnable()
         {
             InputSystem.onDeviceChange += OnDeviceChange;
         }
 
-        void OnDisable()
+        private void OnDisable()
         {
             InputSystem.onDeviceChange -= OnDeviceChange;
         }
 
-        void OnDeviceChange(InputDevice device, InputDeviceChange change)
+        private void OnDeviceChange(InputDevice device, InputDeviceChange change)
         {
             if (change == InputDeviceChange.Added) {
 
                 if (device is Gamepad) {
 
-                    if (_currentgamepadsnumber == _gamepads.Capacity) { return; }
+                    if (_currentGamepadsNumber == _gamepads.Length) { return; }
 
-                    _gamepads.Add((Gamepad)device);  
-                    _currentgamepadsnumber++;
+                    _gamepads[Array.IndexOf(_gamepads,null)] = (Gamepad)device;
+
+
+                    _currentGamepadsNumber++;
 
                 }
 
@@ -42,8 +44,8 @@ namespace Pong.Systems
                 if (device is Gamepad) {
 
                     if (!_gamepads.Contains(device)) { return; }
-                    _gamepads[_gamepads.IndexOf((Gamepad)device)] = null;
-                    _currentgamepadsnumber--;
+                    _gamepads[Array.IndexOf(_gamepads,(Gamepad)device)] = null;
+                    _currentGamepadsNumber--;
                 }
             
             }
