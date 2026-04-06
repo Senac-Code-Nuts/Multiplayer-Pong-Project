d2a2cb54392903a9a44cc6f2a081087017491675
MaisUmHugo <149391609+MaisUmHugo@users.noreply.github.com>
2026-04-06 18:54:08 -0300

    refactor(ball): adiciona dano configuravel e refatora aplicacao de dano

--- Assets/_Game/Gameplay/Ball/Scripts/Ball.cs
+++ Assets/_Game/Gameplay/Ball/Scripts/Ball.cs
@@ -4,7 +4,12 @@ namespace Pong.Gameplay.Ball
 {
     public class Ball : MonoBehaviour
     {
-        [SerializeField, Range(0f,10f)] private float _speed;
+        [Header("Speed")]
+        [SerializeField, Range(0f, 10f)] private float _speed;
+
+        [Header("Damage")]
+        [SerializeField] private int _damage;
+
         private Vector3 _direction;
         private Rigidbody _rigidBody;
 
@@ -17,7 +22,7 @@ namespace Pong.Gameplay.Ball
 
         private void Launch()
         {
-            float dirX = Random.Range(-1f,1f);
+            float dirX = Random.Range(-1f, 1f);
             float dirZ = Random.Range(-1f, 1f);
 
             _direction = new Vector3(dirX, 0, dirZ).normalized;
@@ -32,11 +37,17 @@ namespace Pong.Gameplay.Ball
         private void OnCollisionEnter(Collision collision)
         {
             Vector3 normal = collision.contacts[0].normal;
-            _direction = Vector3.Reflect(_direction,normal);
+            _direction = Vector3.Reflect(_direction, normal);
 
+            _rigidBody.linearVelocity = _direction * _speed;
 
-            _rigidBody.linearVelocity = _direction * _speed; 
+            var damageable = collision.gameObject.GetComponent<Pong.Gameplay.Actors.IDamageable>();
+
+            if (damageable != null)
+            {
+                damageable.ApplyDamage(_damage);
+            }
+
         }
     }
-
 }
